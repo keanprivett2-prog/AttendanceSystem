4dd55bb062e632bb6ffa412f35bb94659f7c6270
keanprivett2-prog <keanprivett2@gmail.com>
2026-07-27 14:37:22 +0200

    Connect Admin V2 to Firebase

--- js/admin-v2.js
+++ js/admin-v2.js
@@ -1,1 +1,12 @@
+// =====================================
+// Firebase Imports
+// =====================================
 
+import { db } from "../firebase/firebase.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+console.log("✅ Admin V2 Loaded");
