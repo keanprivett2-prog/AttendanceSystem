911f7e3e454f5f81a95e6a322857d1c335a15d83
keanprivett2-prog <keanprivett2@gmail.com>
2026-07-27 09:53:08 +0200

    Update auth.js

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,1 +1,38 @@
+// =====================================
+// Firebase Admin Authentication
+// =====================================
 
+import { auth } from "./firebase.js";
+
+import {
+    signInWithEmailAndPassword,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+
+// Sign in administrator
+async function loginAdmin(email, password) {
+
+    const userCredential =
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+    return userCredential.user;
+}
+
+
+// Sign out administrator
+async function logoutAdmin() {
+
+    await signOut(auth);
+}
+
+
+// Export authentication functions
+export {
+    loginAdmin,
+    logoutAdmin
+};
